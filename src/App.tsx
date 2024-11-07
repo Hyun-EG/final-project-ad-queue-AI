cdaf15a2fb7e91b7bd22010b822e4124fe887891
Hyun-EG <codiee@naver.com>
2024-11-08 01:12:53 +0900

    Feat: Lazy 기능능개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import './styles/main.scss';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
-import ManageEmp from './pages/ManageEmp/ManageEmp';
-import HomeDashBoard from './pages/Dashboard/HomeDashBoard';
-import AdminDashBoard from './pages/Dashboard/AdminDashBoard';
-import SignIn from './pages/Auth/SignIn';
-import FindUser from './pages/Auth/FindUser';
-import SignUp from './pages/Auth/SignUp';
-import Mypage from './pages/MyPage';
-import SignUpRequest from './pages/SignUpRequest';
-import MyTasks from './pages/MyTasks';
-import ManageUser from './pages/ManageUser';
-import IssueAd from './pages/IssueAd/IssueAd';
-import IssueAdResult from './pages/IssueAd/IssueAdResult';
-import SameAd from './pages/SameAd/SameAd';
-import SameAdResult from './pages/SameAd/SameAdResult';
-import ManageTask from './pages/ManageEmp/ManageTask';
-import ManageEmpDetail from './pages/ManageEmp/ManageEmpDetail';
-import PrivateRoute from './components/PrivateRoute';
-import { useEffect, useState } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadUserFromLocalStorage } from './redux/authSlice';
 import { RootState } from './redux/authStore';
-import ErrorPages from './pages/ErrorPages';
-import ManageTaskDone from './pages/ManageEmp/ManageTaskDone';
+import PrivateRoute from './components/PrivateRoute';
+
+const ManageEmp = lazy(() => import('./pages/ManageEmp/ManageEmp'));
+const HomeDashBoard = lazy(() => import('./pages/Dashboard/HomeDashBoard'));
+const AdminDashBoard = lazy(() => import('./pages/Dashboard/AdminDashBoard'));
+const SignIn = lazy(() => import('./pages/Auth/SignIn'));
+const FindUser = lazy(() => import('./pages/Auth/FindUser'));
+const SignUp = lazy(() => import('./pages/Auth/SignUp'));
+const Mypage = lazy(() => import('./pages/MyPage'));
+const SignUpRequest = lazy(() => import('./pages/SignUpRequest'));
+const MyTasks = lazy(() => import('./pages/MyTasks'));
+const ManageUser = lazy(() => import('./pages/ManageUser'));
+const IssueAd = lazy(() => import('./pages/IssueAd/IssueAd'));
+const IssueAdResult = lazy(() => import('./pages/IssueAd/IssueAdResult'));
+const SameAd = lazy(() => import('./pages/SameAd/SameAd'));
+const SameAdResult = lazy(() => import('./pages/SameAd/SameAdResult'));
+const ManageTask = lazy(() => import('./pages/ManageEmp/ManageTask'));
+const ManageEmpDetail = lazy(() => import('./pages/ManageEmp/ManageEmpDetail'));
+const ManageTaskDone = lazy(() => import('./pages/ManageEmp/ManageTaskDone'));
+const ErrorPages = lazy(() => import('./pages/ErrorPages'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -40,38 +41,40 @@ const App = () => {
   }
 
   return (
-    <Routes>
-      <Route path="/find-user" element={<FindUser />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/" element={<SignIn />} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/find-user" element={<FindUser />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<SignIn />} />
 
-      <Route
-        path="/*"
-        element={
-          <PrivateRoute>
-            <Layout>
-              <Routes>
-                <Route path="/dashboard" element={<HomeDashBoard />} />
-                <Route path="/dashboard/admin" element={<AdminDashBoard />} />
-                <Route path="admin/manage-emp" element={<ManageEmp />} />
-                <Route path="/employee/:employeeId" element={<ManageEmpDetail />} />
-                <Route path="/admin/manage-task" element={<ManageTask />} />
-                <Route path="/admin/manage-task-done" element={<ManageTaskDone />} />
-                <Route path="/mypage" element={<Mypage />} />
-                <Route path="/admin/approve-user" element={<SignUpRequest />} />
-                <Route path="/admin/manage-user" element={<ManageUser />} />
-                <Route path="/my-task" element={<MyTasks />} />
-                <Route path="/same-ad" element={<SameAd />} />
-                <Route path="/same-ad/result" element={<SameAdResult />} />
-                <Route path="/issue-ad" element={<IssueAd />} />
-                <Route path="/issue-ad/result" element={<IssueAdResult />} />
-                <Route path="*" element={<ErrorPages />} />
-              </Routes>
-            </Layout>
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Routes>
+                  <Route path="/dashboard" element={<HomeDashBoard />} />
+                  <Route path="/dashboard/admin" element={<AdminDashBoard />} />
+                  <Route path="admin/manage-emp" element={<ManageEmp />} />
+                  <Route path="/employee/:employeeId" element={<ManageEmpDetail />} />
+                  <Route path="/admin/manage-task" element={<ManageTask />} />
+                  <Route path="/admin/manage-task-done" element={<ManageTaskDone />} />
+                  <Route path="/mypage" element={<Mypage />} />
+                  <Route path="/admin/approve-user" element={<SignUpRequest />} />
+                  <Route path="/admin/manage-user" element={<ManageUser />} />
+                  <Route path="/my-task" element={<MyTasks />} />
+                  <Route path="/same-ad" element={<SameAd />} />
+                  <Route path="/same-ad/result" element={<SameAdResult />} />
+                  <Route path="/issue-ad" element={<IssueAd />} />
+                  <Route path="/issue-ad/result" element={<IssueAdResult />} />
+                  <Route path="*" element={<ErrorPages />} />
+                </Routes>
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Suspense>
   );
 };
 
